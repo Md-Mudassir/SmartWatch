ad54df4f86f6e56cee3f263644e8b257bdcc1add
Md-Mudassir <mdmudassirpro@gmail.com>
2019-09-06 12:50:34 +0530

    updates

--- js/function.js
+++ js/function.js
@@ -28,3 +28,52 @@ function Time() {
   x.innerHTML = h + ":" + m;
   y.innerHTML = weekdays[weekday_value];
 }
+
+let watch = document.getElementsByClassName("digits")[0],
+  start = document.getElementById("timerplay"),
+  stop = document.getElementById("stop"),
+  clear = document.getElementById("clear"),
+  seconds = 0,
+  minutes = 0,
+  hours = 0,
+  t;
+
+function add() {
+  seconds++;
+  if (seconds >= 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes >= 60) {
+      minutes = 0;
+      hours++;
+    }
+  }
+
+  watch.textContent =
+    (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
+    ":" +
+    (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
+    ":" +
+    (seconds > 9 ? seconds : "0" + seconds);
+
+  timer();
+}
+function timer() {
+  t = setTimeout(add, 1000);
+}
+
+/* Start button */
+timerplay.onclick = timer;
+
+/* Stop button */
+stop.onclick = function() {
+  clearTimeout(t);
+};
+
+/* Clear button */
+reset.onclick = function() {
+  watch.textContent = "00:00:00";
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+};
